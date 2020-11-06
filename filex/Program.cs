b59149158bf4f057a5104ffe8189b06320332797
Jarred Capellman <jcapellman@hotmail.com>
2020-11-05 19:02:19 -0600

    Added: Initial commit

---
+++ filex/Program.cs
@@ -0,0 +1,42 @@
+﻿ using System;
+ using System.IO;
+ using System.Linq;
+
+ namespace filex
+{
+    class Program
+    {
+        private static string ParseArgument(string[] args)
+        {
+            if (args == null)
+            {
+                return null;
+            }
+
+            if (!args.Any())
+            {
+                return null;
+            }
+
+            var file = args[0];
+
+            if (!File.Exists(file))
+            {
+                return null;
+            }
+
+            return file;
+        }
+
+        static void Main(string[] args)
+        {
+            var fileName = ParseArgument(args);
+
+            // Feature Extractor
+
+            // Model Run
+
+            // Model Output
+        }
+    }
+}
