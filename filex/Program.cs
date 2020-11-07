93cb7bdd6bcd9d1720945164dc68f2e09eb94db9
Jarred Capellman <jcapellman@hotmail.com>
2020-11-06 18:12:51 -0600

    Added: Exception and XML Doc

--- filex/Program.cs
+++ filex/Program.cs
@@ -6,23 +6,31 @@
 {
     class Program
     {
+        /// <summary>
+        /// Parses the command line argument
+        /// </summary>
+        /// <param name="args">Contains the file name to scan</param>
+        /// <returns>Filename of the file to scan</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when args is null</exception>
+        /// <exception cref="System.IO.FileNotFoundException">Throw when the file is not found</exception>
+        /// <exception cref="System.ArgumentException">Thrown when args is empty</exception>
         private static string ParseArgument(string[] args)
         {
             if (args == null)
             {
-                return null;
+                throw new ArgumentNullException(nameof(args));
             }
 
             if (!args.Any())
             {
-                return null;
+                throw new ArgumentException("Args was empty");
             }
 
             var file = args[0];
 
             if (!File.Exists(file))
             {
-                return null;
+                throw new FileNotFoundException($"File: {file} does not exist");
             }
 
             return file;
