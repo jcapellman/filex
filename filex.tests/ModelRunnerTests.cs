4170aa1d08697bc3d935b04186cb4e1abf952e3f
Jarred Capellman <jcapellman@hotmail.com>
2020-11-07 19:20:22 -0600

    Added: Initial work on the ModelRunnerTests class

---
+++ filex.tests/ModelRunnerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace filex.tests
+{
+    [TestClass]
+    public class ModelRunnerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void ModelNotFound()
+        {
+            var model = new ModelRunner("Testing");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ModelNullFound()
+        {
+            var model = new ModelRunner(null);
+        }
+    }
+}
