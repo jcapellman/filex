e1d122aaf20dab0db727486e03270380b8fd01d2
Jarred Capellman <jcapellman@hotmail.com>
2020-11-09 19:27:26 -0600

    Added: BaseArgument Abstract Class

---
+++ filex/Arguments/Base/BaseArgument.cs
@@ -0,0 +1,13 @@
+﻿namespace filex.Arguments.Base
+{
+    public abstract class BaseArgument
+    {
+        public abstract string Argument { get; }
+
+        public abstract string PropertyMap { get; }
+
+        public abstract bool ValidArgument(string argValue);
+
+        public abstract object GetValue(string argValue);
+    }
+}
