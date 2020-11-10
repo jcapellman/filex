3a3ad0750e8b367932ab4c6ed08a01534ff54ac4
Jarred Capellman <jcapellman@hotmail.com>
2020-11-10 11:38:06 -0600

    Added: VerboseArgument class

---
+++ filex/Arguments/VerboseArgument.cs
@@ -0,0 +1,16 @@
+﻿using filex.Arguments.Base;
+using filex.Objects;
+
+namespace filex.Arguments
+{
+    public class VerboseArgument : BaseArgument
+    {
+        public override string Argument => "verbose";
+
+        public override string PropertyMap => nameof(ArgumentResponseItem.Verbose);
+
+        public override bool ValidArgument(string argValue) => bool.TryParse(argValue, out var _);
+
+        public override object GetValue(string argValue) => bool.Parse(argValue);
+    }
+}
