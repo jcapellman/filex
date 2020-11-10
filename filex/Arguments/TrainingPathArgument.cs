01501c5052f33c85de77da570cdd722b25b97a94
Jarred Capellman <jcapellman@hotmail.com>
2020-11-10 12:28:55 -0600

    Added: TrainingPathArgument class

---
+++ filex/Arguments/TrainingPathArgument.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+using filex.Arguments.Base;
+using filex.Objects;
+
+namespace filex.Arguments
+{
+    public class TrainingPathArgument : BaseArgument
+    {
+        public override string Argument => "trainingpath";
+
+        public override object DefaultValue => string.Empty;
+
+        public override string PropertyMap => nameof(ArgumentResponseItem.TrainingPath);
+
+        public override bool ValidArgument(string argValue) => !string.IsNullOrEmpty(argValue) && Directory.Exists(argValue);
+
+        public override object GetValue(string argValue) => argValue;
+    }
+}
