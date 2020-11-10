039a2fa893d5a69cf3ad485ac8083fa5128ce10a
Jarred Capellman <jcapellman@hotmail.com>
2020-11-09 19:27:58 -0600

    Added: FilePath Argument Class

---
+++ filex/Arguments/FilePath.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+using filex.Arguments.Base;
+using filex.Objects;
+
+namespace filex.Arguments
+{
+    public class FilePath : BaseArgument
+    {
+        public override string Argument => "file";
+
+        public override string PropertyMap => nameof(ArgumentResponseItem.FileNameForClassification);
+
+        public override bool ValidArgument(string argValue) => !string.IsNullOrEmpty(argValue) && File.Exists(argValue);
+
+        public override object GetValue(string argValue) => argValue;
+    }
+}
