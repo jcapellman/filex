93589df55ee125f91b92c92ed5fbaf7de6f03fa0
Jarred Capellman <jcapellman@hotmail.com>
2020-11-13 07:17:36 -0500

    Added: VersionArgument class

---
+++ filex/Arguments/VersionArgument.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+using filex.Arguments.Base;
+
+namespace filex.Arguments
+{
+    public class VersionArgument : BaseArgument
+    {
+        public override string Argument => "version";
+
+        public override string UsageText => "Retrieves the version of the application";
+
+        public override object DefaultValue => null;
+
+        public override string PropertyMap => null;
+
+        public override bool ValidArgument(string argValue) => false;
+
+        public override object GetValue(string argValue) => Assembly.GetExecutingAssembly().GetName().Version;
+    }
+}
