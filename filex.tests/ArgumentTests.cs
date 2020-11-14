8688c7fbda9e3059bf97c57ecd04917aa09b9754
Jarred Capellman <jcapellman@hotmail.com>
2020-11-14 16:00:17 -0500

    Change: Migrated the VersionArgumentTests to its own class

--- filex.tests/ArgumentTests.cs
+++ filex.tests/ArgumentTests.cs
@@ -43,59 +43,7 @@ namespace filex.tests
             Assert.IsTrue(parsed.Any());
         }
 
-        [TestMethod]
-        public void VersionArgumentChecks()
-        {
-            var parsed = ArgumentParser.Parse(new []{"version"});
-
-            Assert.IsNotNull(parsed);
-            Assert.IsFalse(parsed.ValidOption);
-        }
-
-        [TestMethod]
-        public void VersionUsageText()
-        {
-            var verArgument = new VersionArgument();
-
-            Assert.IsNotNull(verArgument.UsageText);
-        }
-
-
-        [TestMethod]
-        [ExpectedException(typeof(NotImplementedException))]
-        public void VersionArgumentNotImplementedDefaultValue()
-        {
-            var version = new VersionArgument();
-
-            var val = version.DefaultValue;
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(NotImplementedException))]
-        public void VersionArgumentNotImplementedPropertyMap()
-        {
-            var version = new VersionArgument();
-
-            var val = version.PropertyMap;
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(NotImplementedException))]
-        public void VersionArgumentValidOption()
-        {
-            var version = new VersionArgument();
-            
-            version.ValidArgument(string.Empty);
-        }
-
-        [TestMethod]
-        public void VersionArgumentGetValue()
-        {
-            var version = new VersionArgument();
-
-            Assert.IsNotNull(version.GetValue(string.Empty));
-        }
-
+        
         [TestMethod]
         public void InvalidBlankspaceOption()
         {
