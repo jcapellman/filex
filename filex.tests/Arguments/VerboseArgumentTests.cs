70ae6cab42336ce5b2be93bf688874f115672a05
Jarred Capellman <jcapellman@hotmail.com>
2020-11-14 16:22:36 -0500

    Added: VerboseArgumentTests Class and additional coverage

---
+++ filex.tests/Arguments/VerboseArgumentTests.cs
@@ -0,0 +1,18 @@
+﻿using filex.Arguments;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace filex.tests.Arguments
+{
+    [TestClass]
+    public class VerboseArgumentTests
+    {
+        [TestMethod]
+        public void VerboseArgument_UsageText()
+        {
+            var vArgument = new VerboseArgument();
+
+            Assert.IsNotNull(vArgument.UsageText);
+        }
+    }
+}
