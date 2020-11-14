6b4074213ee4b7eeada0481348b9a9a9fbebb38c
Jarred Capellman <jcapellman@hotmail.com>
2020-11-14 16:17:11 -0500

    Added: OperationModeArgumentTests class and additional coverage

---
+++ filex.tests/Arguments/OperationModeArgumentTests.cs
@@ -0,0 +1,18 @@
+﻿using filex.Arguments;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace filex.tests.Arguments
+{
+    [TestClass]
+    public class OperationModeArgumentTests
+    {
+        [TestMethod]
+        public void OperationModeArgument_UsageText()
+        {
+            var omArgument = new OperationModeArgument();
+
+            Assert.IsNotNull(omArgument.UsageText);
+        }
+    }
+}
