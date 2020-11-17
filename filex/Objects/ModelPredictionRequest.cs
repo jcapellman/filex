ae4bf8db568a92fa2f6869c39929549a34ad3e7d
Jarred Capellman <jcapellman@hotmail.com>
2020-11-16 19:36:11 -0600

    Added: ModelPredictionRequest Class

---
+++ filex/Objects/ModelPredictionRequest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ML.Data;
+
+namespace filex.Objects
+{
+    public class ModelPredictionRequest
+    {
+        [LoadColumn(0)]
+        public float FileSize { get; set; }
+
+        [LoadColumn(1)]
+        public float IsPE { get; set; }
+
+        [LoadColumn(2)]
+        public bool Label { get; set; }
+    }
+}
