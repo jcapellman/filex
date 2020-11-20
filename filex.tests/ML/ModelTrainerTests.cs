6352eebd842410654f1ce7b3deeaa9564130abdc
Jarred Capellman <jcapellman@hotmail.com>
2020-11-19 20:36:34 -0600

    Added: ModelTrainerTests class

---
+++ filex.tests/ML/ModelTrainerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+using filex.ML;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace filex.tests.ML
+{
+    [TestClass]
+    public class ModelTrainerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ModelTrainer_FeatureExtraction_NullPath()
+        {
+            ModelTrainer.FeatureExtraction(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DirectoryNotFoundException))]
+        public void ModelTrainer_FeatureExtraction_FNF()
+        {
+            ModelTrainer.FeatureExtraction("nullo");
+        }
+
+        [TestMethod]
+        public void ModelTrainer_FeatureExtraction_ValidPath()
+        {
+            var files = ModelTrainer.FeatureExtraction(Environment.GetEnvironmentVariable("windir"));
+
+            Assert.IsTrue(files.Any());
+        }
+    }
+}
