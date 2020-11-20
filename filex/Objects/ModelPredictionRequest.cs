697fc41726793eb1f6c00d4dcaa37365867a2272
Jarred Capellman <jcapellman@hotmail.com>
2020-11-19 19:44:30 -0600

    Change: Refactored ModelPredictionRequest to load all of the properties

--- filex/Objects/ModelPredictionRequest.cs
+++ filex/Objects/ModelPredictionRequest.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.ML.Data;
+﻿using System;
+using System.IO;
+
+using Microsoft.ML.Data;
 
 namespace filex.Objects
 {
@@ -12,5 +15,26 @@ namespace filex.Objects
 
         [LoadColumn(2)]
         public bool Label { get; set; }
+
+        public ModelPredictionRequest(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException($"Could not find {fileName}");
+            }
+
+            Label = fileName.Contains("benign");
+
+            var fileBytes = File.ReadAllBytes(fileName);
+
+            IsPE = System.Text.Encoding.ASCII.GetString(fileBytes.AsSpan().Slice(0, 2)) == "MZ" ? 1.0f : 0.0f;
+
+            FileSize = fileBytes.Length;
+        }
     }
 }
