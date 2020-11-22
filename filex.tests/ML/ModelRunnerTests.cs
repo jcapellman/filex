0681a8cbac9589318fad1edab98f14255eb1dd4d
Jarred Capellman <jcapellman@hotmail.com>
2020-11-21 22:11:49 -0600

    Added: Unit Tests for the ModelRunner class

--- filex.tests/ML/ModelRunnerTests.cs
+++ filex.tests/ML/ModelRunnerTests.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using filex.Arguments.Base;
+using filex.Objects;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace filex.tests.ML
@@ -9,16 +13,41 @@ namespace filex.tests.ML
     {
         [TestMethod]
         [ExpectedException(typeof(FileNotFoundException))]
-        public void ModelNotFound()
+        public void ModelRunner_NotFound()
         {
             var model = new ModelRunner("Testing");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void ModelNullFound()
+        public void ModelRunner_NullFilename()
         {
             var model = new ModelRunner(null);
+        }
+
+        [TestMethod]
+        public void ModelRunner_FileExistsValidModelRun()
+        {
+            var file = Path.GetRandomFileName();
+
+            File.WriteAllText(file, "");
+
+            var model = new ModelRunner(file);
+
+            var response = model.RunModel(new ArgumentResponseItem(new List<BaseArgument>()));
+
+            Assert.IsNotNull(response);
+            Assert.IsFalse(response.Prediction);
+        }
+
+        [TestMethod]
+        public void ModelRunner_FileExistsButInvalidModel()
+        {
+            var file = Path.GetRandomFileName();
+
+            File.WriteAllText(file, "");
+
+            var model = new ModelRunner(file);
         }
     }
 }
