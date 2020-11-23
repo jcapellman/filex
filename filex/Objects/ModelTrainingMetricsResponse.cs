adf71018a82c25084faf89d1c1885391dab31b70
Jarred Capellman <jcapellman@hotmail.com>
2020-11-22 22:23:37 -0600

    Added: Initial work on the ModelTrainingMetricsResponse class

---
+++ filex/Objects/ModelTrainingMetricsResponse.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.ML.Data;
+
+namespace filex.Objects
+{
+    public class ModelTrainingMetricsResponse
+    {
+        public double AUC { get; private set; }
+
+        public double Entropy { get; private set; }
+
+        public ModelTrainingMetricsResponse(CalibratedBinaryClassificationMetrics metric)
+        {
+            AUC = metric.AreaUnderRocCurve;
+            Entropy = metric.Entropy;
+        }
+    }
+}
