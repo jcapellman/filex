8c8e3e2c53dc97c7d92603494e37e58872303959
Jarred Capellman <jcapellman@hotmail.com>
2020-11-24 21:37:39 -0600

    Added: Initial work on the BaseParser class

---
+++ filex/Parsers/Base/BaseParser.cs
@@ -0,0 +1,9 @@
+﻿namespace filex.Parsers.Base
+{
+    public abstract class BaseParser
+    {
+        public abstract string Name { get; }
+
+        public abstract bool IsParsable(byte[] data);
+    }
+}
