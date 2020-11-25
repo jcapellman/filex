22645120e3428e2707aa6c1201e544ec93d6c692
Jarred Capellman <jcapellman@hotmail.com>
2020-11-25 09:16:16 -0600

    Added: Initial PCAPParser Class

---
+++ filex/Parsers/PCAPParser.cs
@@ -0,0 +1,14 @@
+﻿using filex.Parsers.Base;
+
+namespace filex.Parsers
+{
+    public class PCAPParser : BaseParser
+    {
+        public override string Name => "PCAP";
+
+        public override bool IsParseable(byte[] data)
+        {
+            return false;
+        }
+    }
+}
