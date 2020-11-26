78fa8c144b7ae3a5931db5bba7f023e389dc8ab5
Jarred Capellman <jcapellman@hotmail.com>
2020-11-25 19:38:21 -0600

    Added: RunModel to the BaseParser class

--- filex/Parsers/Base/BaseParser.cs
+++ filex/Parsers/Base/BaseParser.cs
@@ -1,9 +1,13 @@
-﻿namespace filex.Parsers.Base
+﻿using filex.Objects;
+
+namespace filex.Parsers.Base
 {
     public abstract class BaseParser
     {
         public abstract string Name { get; }
 
         public abstract bool IsParseable(byte[] data, string fileName);
+
+        public abstract ModelPredictionResponse RunModel(byte[] data, string fileName);
     }
 }
