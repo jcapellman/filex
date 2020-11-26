f08edbf9b877e739d65b6b093bd22592dc281b1e
Jarred Capellman <jcapellman@hotmail.com>
2020-11-25 19:40:10 -0600

    Added: Initial work on the PCAPParserTests class

---
+++ filex.tests/Parsers/PCAPParserTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using filex.Parsers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace filex.tests.Parsers
+{
+    [TestClass]
+    public class PCAPParserTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PCAPParser_NullArguments()
+        {
+            var pParser = new PCAPParser();
+
+            pParser.IsParseable(null, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PCAPParser_FNFArguments()
+        {
+            var pParser = new PCAPParser();
+
+            pParser.IsParseable(null, "wick");
+        }
+    }
+}
