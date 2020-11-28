5b563b0311ed5ff5d9b0f20e849b30e787e491e3
Jarred Capellman <jcapellman@hotmail.com>
2020-11-27 18:56:01 -0600

    Added: TrainModel method in the PCAPParser class

--- filex/Parsers/PCAP/PCAPParser.cs
+++ filex/Parsers/PCAP/PCAPParser.cs
@@ -81,10 +81,7 @@ namespace filex.Parsers.PCAP
             var packet = Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
             var ethernetPacket = (EthernetPacket) packet;
 
-            var requestItem = new PCAPFeatureExtractionRequestItem
-            {
-                PayloadContent = Encoding.ASCII.GetString(ethernetPacket.PayloadPacket.Bytes)
-            };
+            var requestItem = new PCAPFeatureExtractionRequestItem(ethernetPacket.PayloadPacket.Bytes);
 
             _packetPredictions.Add(_mlEngine.Predict(requestItem));
         }
@@ -106,6 +103,45 @@ namespace filex.Parsers.PCAP
             var model = mlContext.Model.Load(MODEL_NAME, out _);
 
             _mlEngine = mlContext.Model.CreatePredictionEngine<PCAPFeatureExtractionRequestItem, ModelPredictionResponse>(model);
+        }
+
+        public override void TrainModel(string trainingPath)
+        {
+            if (string.IsNullOrEmpty(trainingPath))
+            {
+                throw new ArgumentNullException($"TrainingDataPath was not set");
+            }
+
+            var mlContext = new MLContext(Constants.ML_SEED);
+
+            var files = GetFiles(trainingPath);
+
+            var data = new List<PCAPFeatureExtractionRequestItem>();
+
+            foreach (var file in files)
+            {
+                //    data.Add(RunModel(null, file));
+            }
+
+            var dataView = mlContext.Data.LoadFromEnumerable(data);
+
+            var split = mlContext.Data.TrainTestSplit(dataView, testFraction: .5);
+
+            var pipeline = mlContext.Transforms.Concatenate(
+                    "Features", nameof(PCAPFeatureExtractionRequestItem.PayloadContent))
+                .Append(mlContext.AnomalyDetection.Trainers.RandomizedPca(featureColumnName: "Features"));
+
+            ITransformer trainedModel = pipeline.Fit(split.TrainSet);
+
+            var predictions = trainedModel.Transform(split.TestSet);
+
+            mlContext.Model.Save(trainedModel, predictions.Schema, MODEL_NAME);
+
+            var metrics = mlContext.AnomalyDetection.Evaluate(
+                data: predictions,
+                labelColumnName: nameof(ModelPredictionRequest.Label),
+                scoreColumnName: "Score",
+                predictedLabelColumnName: nameof(ModelPredictionRequest.Label));
         }
     }
 }
