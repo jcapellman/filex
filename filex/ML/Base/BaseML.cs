fd26d29ab9e1014aae6c2d94d928f3dab089b564
Jarred Capellman <jcapellman@hotmail.com>
2020-11-28 14:48:49 -0600

    Added: Initial work on the BaseML Class

---
+++ filex/ML/Base/BaseML.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using filex.Parsers.Base;
+
+namespace filex.ML.Base
+{
+    public class BaseML
+    {
+        protected readonly List<BaseParser> _parsers;
+
+        public BaseML()
+        {
+            _parsers = GetType().Assembly.GetTypes().Where(a => a.BaseType == typeof(BaseParser) && !a.IsAbstract)
+                .Select(b => (BaseParser)Activator.CreateInstance(b)).ToList();
+        }
+    }
+}
