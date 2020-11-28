4ef74eb9bc1cc6c62249e4a5d3b604f3a62d2951
Jarred Capellman <jcapellman@hotmail.com>
2020-11-27 18:53:40 -0600

    Added: GetFiles method to the BaseParser

--- filex/Parsers/Base/BaseParser.cs
+++ filex/Parsers/Base/BaseParser.cs
@@ -1,4 +1,8 @@
-﻿using filex.Objects;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using filex.Objects;
 
 namespace filex.Parsers.Base
 {
@@ -11,5 +15,22 @@ namespace filex.Parsers.Base
         public abstract ModelPredictionResponse RunModel(byte[] data, string fileName);
 
         public abstract void LoadModel();
+
+        public abstract void TrainModel(string trainingPath);
+
+        protected static IEnumerable<string> GetFiles(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentNullException($"Path was not set");
+            }
+
+            if (!Directory.Exists(path))
+            {
+                throw new DirectoryNotFoundException($"Path: {path} not found");
+            }
+
+            return Directory.GetFiles(path);
+        }
     }
 }
