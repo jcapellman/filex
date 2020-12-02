0d1f593f3c071e4f8360e54d51122303c343468a
Jarred Capellman <jcapellman@hotmail.com>
2020-12-01 20:45:36 -0600

    Added: Additional Unit Tests to cover the PCAPParser class

--- filex.tests/Parsers/PCAPParserTests.cs
+++ filex.tests/Parsers/PCAPParserTests.cs
@@ -39,17 +39,51 @@ namespace filex.tests.Parsers
         }
 
         [TestMethod]
-        public void PCAPParser_BenginPCAPArguments()
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PCAPParser_Train_NullPath()
+        {
+            var pParser = new PCAPParser();
+
+            var result = pParser.TrainModel(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DirectoryNotFoundException))]
+        public void PCAPParser_Train_InvalidPath()
+        {
+            var pParser = new PCAPParser();
+
+            var result = pParser.TrainModel(@"c:\wicko");
+        }
+
+        [TestMethod]
+        public void PCAPParser_Train_ValidPath()
+        {
+            var pParser = new PCAPParser();
+
+            var path = Path.Combine(AppContext.BaseDirectory, @"..\..\..\Samples\pcap\");
+
+            var result = pParser.TrainModel(path);
+
+            Assert.IsTrue(result.AUC == 0.0);
+
+            Assert.IsTrue(result.F1Score == 0.0);
+        }
+
+        [TestMethod]
+        public void ddPCAPParser_BenginPCAPArguments()
         {
             var pParser = new PCAPParser();
 
             Assert.IsTrue(pParser.Name == "PCAP");
 
-            var path = Path.Combine(AppContext.BaseDirectory, @"..\..\..\Samples\benign_pcap.pcap");
+            var path = Path.Combine(AppContext.BaseDirectory, @"..\..\..\Samples\pcap\benign_pcap.pcap");
 
             var result = pParser.IsParseable(null, path);
 
             Assert.IsTrue(result);
+
+            pParser.LoadModel();
 
             var modelRun = pParser.RunModel(null, path);
 
