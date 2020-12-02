ff34f5a8d0c9cdb364ab3ce9d097b1667a57baf7
Jarred Capellman <jcapellman@hotmail.com>
2020-12-01 20:47:32 -0600

    Added: Label and NgramFeatures properties in the PCAPFeatureExtractionRequestItem class

--- filex/Parsers/PCAP/Objects/PCAPFeatureExtractionRequestItem.cs
+++ filex/Parsers/PCAP/Objects/PCAPFeatureExtractionRequestItem.cs
@@ -7,11 +7,16 @@ namespace filex.Parsers.PCAP.Objects
     {
         public string PayloadContent { get; set; }
 
-        public PCAPFeatureExtractionRequestItem(byte[] packet)
+        public float[] NgramFeatures { get; set; }
+
+        public float Label { get; set; }
+
+        public PCAPFeatureExtractionRequestItem(byte[] packet, bool label = true)
         {
             PayloadContent = Encoding.ASCII.GetString(packet);
+            Label = label ? 1.0f : 0.0f;
         }
 
-        public PCAPFeatureExtractionRequestItem(string fileName) : this(File.ReadAllBytes(fileName)) { }
+        public PCAPFeatureExtractionRequestItem(string fileName) : this(File.ReadAllBytes(fileName), fileName.Contains("benign")) { }
     }
 }
